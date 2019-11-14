ac0b4e4e869e92b3d2f2b7665d170824464a0540
karta571110 <karta571110@gmail.com>
2019-11-14 15:21:48 +0800

    finish

--- app/src/main/java/com/example/androidhw14/MainActivity.java
+++ app/src/main/java/com/example/androidhw14/MainActivity.java
@@ -16,11 +16,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button phonebtn=(Button)findViewById(R.id.phone);
+        Button mapbtn=(Button)findViewById(R.id.map);
+        Button webbtn=(Button)findViewById(R.id.web);
         phonebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(Intent.ACTION_DIAL);//也可以   Intent intent=new Intent(Intent.ACTION_DIAL,Uri.parse("tel:0986347652")); 這樣的話就不用下面那行了
                 intent.setData(Uri.parse("tel:0900064037"));
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }
+            }
+        });
+        mapbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("geo:23,-122"));
+                intent.setPackage("com.google.android.apps.maps");
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }
+
+            }
+        });
+        webbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com/"));
                 if(intent.resolveActivity(getPackageManager())!=null){
                     startActivity(intent);
                 }
